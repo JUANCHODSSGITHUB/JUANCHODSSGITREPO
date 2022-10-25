63e3d3c9e43f4c17cfff2b29eceebfd4f4d9114a
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 14:07:55 +0800

    [create] DSS4-MS-MOVIE-V1

---
+++ DSS4-MS-MOVIE-V1/src/test/java/com/example/dss4msmoviev1/Dss4MsMovieV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss4msmoviev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss4MsMovieV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
