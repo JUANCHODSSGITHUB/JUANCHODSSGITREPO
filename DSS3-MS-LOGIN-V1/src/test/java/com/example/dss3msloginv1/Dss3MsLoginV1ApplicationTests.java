b0815ec95d728848ca01b0a48f7f25e6d264b79a
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 14:03:50 +0800

    [create] DSS3-MS-LOGIN-V1

---
+++ DSS3-MS-LOGIN-V1/src/test/java/com/example/dss3msloginv1/Dss3MsLoginV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss3msloginv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss3MsLoginV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
