cdd86978c98b577de8e9fe0879e1f1d88a3042cb
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 14:09:43 +0800

    [create] DSS5-MS-ACTOR-V1

---
+++ DSS5-MS-ACTOR-V1/src/main/java/com/example/dss5msactorv1/Dss5MsActorV1Application.java
@@ -0,0 +1,13 @@
+package com.example.dss5msactorv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss5MsActorV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dss5MsActorV1Application.class, args);
+    }
+
+}
---
+++ DSS5-MS-ACTOR-V1/src/test/java/com/example/dss5msactorv1/Dss5MsActorV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss5msactorv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss5MsActorV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
