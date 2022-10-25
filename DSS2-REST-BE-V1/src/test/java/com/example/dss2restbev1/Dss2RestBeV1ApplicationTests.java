b617e53b416208d4cc3f1b6d6c037d756f937375
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 14:01:36 +0800

    [create] DSS2-REST-BE-V1

---
+++ DSS2-REST-BE-V1/src/test/java/com/example/dss2restbev1/Dss2RestBeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss2restbev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss2RestBeV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
