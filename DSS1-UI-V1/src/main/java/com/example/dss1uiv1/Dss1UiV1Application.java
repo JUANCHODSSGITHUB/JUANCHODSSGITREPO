7f67a6e18c2fe0c9a2d196d18081f5fec8f264ba
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 13:58:33 +0800

    [create] DSS1-UI-V1

---
+++ DSS1-UI-V1/src/main/java/com/example/dss1uiv1/Dss1UiV1Application.java
@@ -0,0 +1,13 @@
+package com.example.dss1uiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss1UiV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dss1UiV1Application.class, args);
+    }
+
+}
---
+++ DSS1-UI-V1/src/test/java/com/example/dss1uiv1/Dss1UiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss1uiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss1UiV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
