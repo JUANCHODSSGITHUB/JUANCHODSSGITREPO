e67f56a3c1a37fb3580a88eea0eafd88a11cbbce
JUANCHODSSGITHUB <juanchodssgithub@gmail.com>
2022-10-25 14:10:52 +0800

    [create] DSS6-MS-REVIEW-V1

---
+++ DSS6-MS-REVIEW-V1/src/main/java/com/example/dss6msreviewv1/Dss6MsReviewV1Application.java
@@ -0,0 +1,13 @@
+package com.example.dss6msreviewv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss6MsReviewV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dss6MsReviewV1Application.class, args);
+    }
+
+}
---
+++ DSS6-MS-REVIEW-V1/src/test/java/com/example/dss6msreviewv1/Dss6MsReviewV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dss6msreviewv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss6MsReviewV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
